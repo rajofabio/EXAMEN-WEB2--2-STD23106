a2715235353f4d0778a3718a122afcd751bfb1c0
rajofabio <hei.fabio.2@gmail.com>
2024-09-07 00:24:11 +0300

    fix: change base url

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://examen-web2-2-std23106-backend.onrender.com";
 
 export const getPossessions = () => axios.get(`${API_BASE_URL}/possession`);
 
